2c744bd4ed10805b3fbce74a42e5aec7c4912131
kevin940726 <kevin830726@gmail.com>
2017-02-22 23:37:12 +0800

    Add appledaily authors, fix content to exclude author

--- src/sites/appledaily.js
+++ src/sites/appledaily.js
@@ -4,17 +4,26 @@ async function appledaily(fetch, cheerio, url) {
     .then(body => cheerio.load(body));
 
   const dateRegex = /(\d{4})年(\d{2})月(\d{2})日(\d{2}):(\d{2})/g;
+  const authorsRegex = /（(.*)／.*報導）/g;
 
   const title = $('#h1').text();
-  const content = $('#summary').text();
+  const content = $('#summary').text().replace(authorsRegex, '');
 
   const dateGroup = dateRegex.exec($('time').text());
-  const date = new Date(dateGroup[1], +dateGroup[2] - 1, dateGroup[3], dateGroup[4], dateGroup[5]);
+  const date = dateGroup ?
+    new Date(dateGroup[1], +dateGroup[2] - 1, dateGroup[3], dateGroup[4], dateGroup[5]) :
+    null;
+
+  const authorsGroup = authorsRegex.exec($('#summary').text());
+  const authors = authorsGroup ?
+    [authorsGroup[1]] :
+    [];
 
   return {
     title,
     content,
     date,
+    authors,
   };
 }
 
